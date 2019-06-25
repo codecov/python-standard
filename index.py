c504b95816772313264812cd83b7781f406d9563
ibrahim0814 <ibrahim.a.ali@vanderbilt.edu>
2019-06-25 16:03:29 -0700

    initial

---
+++ index.py
@@ -0,0 +1,13 @@
+def add(x,y):
+    return x+y
+
+def subtract(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    if y==0:
+        raise ValueError('You are dividing by goose eggs!')
+    return x/y
