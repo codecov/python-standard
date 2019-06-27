c3c5230d357ae94bc2fd7c06398f08acf94ffc35
ibrahim0814 <ibrahim.0814@gmail.com>
2019-06-27 16:26:58 -0700

    add html and xml coverage files

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,8 @@
 FROM python:3.6
 
-RUN pip install pytest
+RUN pip install codecov \
+&& pip install pytest \
+&& pip install pytest-cov
 
 WORKDIR /app
-
 COPY . /app
-
-RUN py.test
--- test_index.py
+++ test_index.py
@@ -1,14 +1,12 @@
 import pytest
 import index
 
-def test_add():
-    assert index.add(2,2) == 4
+def test_uncovered_if():
+    assert index.uncovered_if() == True
 
-def test_subtract():
-    assert index.subtract(4,2) == 2
+def test_fully_covered():
+    assert index.fully_covered() == True
 
-def test_multiply():
-    assert index.multiply(4,2) == 8
 
-def test_divide():
-    assert index.divide(8,4) == 2
+
+
