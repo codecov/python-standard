ef74efff99774fcd70c766200826e16a346e467e
“ibrahim0814” <ibrahim.a.ali@vanderbilt.edu>
2019-07-05 14:31:48 -0700

    add 60 second wait time to request script

--- request.py
+++ request.py
@@ -1,9 +1,12 @@
 #testing to see if I can hit the CC API and return coverage stats
 import requests
 import urllib
+import time
 
 payload = {'token': '11412e01-7ed6-4d72-b438-d8a297eced21'}
 link = 'https://codecov.io/api/gh/ibrahim0814/pystandard-draft'
+
+time.sleep(60)
 
 #get latest coverage data
 all_data = requests.get(link, params=payload).json()
