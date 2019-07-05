a28273ea23e391801c3777ee6a76cbd959ebdf7a
“ibrahim0814” <ibrahim.a.ali@vanderbilt.edu>
2019-07-05 14:31:04 -0700

    change bad assertion statement

--- test_index.py
+++ test_index.py
@@ -2,7 +2,7 @@ import pytest
 import index
 
 def test_uncovered_if():
-    assert index.uncovered_if() == True
+    assert index.uncovered_if() == False
 
 def test_fully_covered():
     assert index.fully_covered() == True
