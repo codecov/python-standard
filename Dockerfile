a6ddd0ea0be1473f30f1ca35599075768fc806b1
“ibrahim0814” <ibrahim.a.ali@vanderbilt.edu>
2019-07-05 14:31:07 -0700

    update with new code

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@ FROM python:3.6
 
 RUN pip install codecov \
 && pip install pytest \
-&& pip install pytest-cov
+&& pip install pytest-cov \
+&& pip install requests
 
 WORKDIR /app
-COPY . /app
+COPY  . /app
---
+++ request.py
@@ -0,0 +1,14 @@
+#testing to see if I can hit the CC API and return coverage stats
+
+import requests
+import urllib
+
+payload = {'token': '2604fdaf998b4196be55430e8cb2b7b9'}
+link = 'https://codecov.io/api/gh/ibrahim0814/codecov-test'
+
+all_data = requests.get(link, params=payload).json()
+commit_data = all_data['commits'][0]
+
+coverage_percentage = commit_data['totals']['c']
+
+print(coverage_percentage)
