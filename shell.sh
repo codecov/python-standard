715b97fd85d4314c794a869bf33d64b30bde5983
Felipe <felipe@codecov.io>
2019-09-13 13:09:48 -0700

    Test storage 1

--- shell.sh
+++ shell.sh
@@ -6,5 +6,5 @@ set -x
 
 export CI="True"
 export APPVEYOR="True"
-export CODECOV_URL="https://54eb73c4.ngrok.io/"
-bash <(curl -s https://54eb73c4.ngrok.io/bash) 
+export CODECOV_URL="https://e68cb227.ngrok.io/"
+bash <(curl -s https://e68cb227.ngrok.io/bash) 
