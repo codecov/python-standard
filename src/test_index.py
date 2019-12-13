1e50678402ca10ab0d5e825e54ef664047ed8b71
Ibrahim Ali <ibrahim.a.ali@vanderbilt.edu>
2019-12-12 23:32:07 -0800

    Update test_index.py

--- src/test_index.py
+++ src/test_index.py
@@ -4,8 +4,8 @@ import index
 def test_uncovered_if():
     assert index.uncovered_if() == False
 
-def test_fully_covered():
-    assert index.fully_covered() == True
+# def test_fully_covered():
+#     assert index.fully_covered() == True
 
 
 
