c28eb9deb29a257de0dcdc96a5991aa977eede1c
Edward Izzo <edward@codecov.io>
2020-04-14 17:47:34 -0400

    ...

--- src/index.py
+++ src/index.py
@@ -27,17 +27,4 @@ def stillUncovered():
 
 def stillUncoveredTwo():
     a = 1
-    b = 2
-    c = 3
-    d = 4
-    e = 5
-    f = 6
-    g = 7
-    h = 8
-    i = 9
-    j = 10
-    k = 11
-    l = 12
-    m = 13
-    n = 14
     return 'nothing two see here'
